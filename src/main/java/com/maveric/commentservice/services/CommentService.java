a320a995d116b3c4a8d59e70ade0d18e348c58b0
deepaksi7041 <deepaksi@maveric-systems.com>
2022-04-01 13:59:14 +0530

    get feature added

--- src/main/java/com/maveric/commentservice/services/CommentService.java
+++ src/main/java/com/maveric/commentservice/services/CommentService.java
@@ -7,6 +7,9 @@ import com.maveric.commentservice.model.Comment;
 import java.util.List;
 
 public interface CommentService {
+	 List<CommentResponse> getComments(String postId);
+	 Integer getCommentsCount(String postId);
+	 CommentResponse getCommentDetails(String postId,String commentId);
     CommentResponse createComment(String postId,Commentdto commentdto);
 	CommentResponse updateComment(String postId,String commentId, Commentdto updateComment);
     String deleteComment(String postId,String commentId);
